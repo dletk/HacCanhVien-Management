b9ab705eef54a11574b584bb8bcd1847e7af4d7c
Duc Le <duc.mtle@gmail.com>
2023-03-26 16:53:52 -0400

    Added missing field to Mo

--- haccanhvien/admin.py
+++ haccanhvien/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import CapDoSanPham, KhuVuc, LoaiHang, LoaiSanPham, Mo
+
 # Register your models here.
+admin.site.register([KhuVuc, LoaiHang, LoaiSanPham, CapDoSanPham, Mo])
+
+
+admin.site.site_header = "Hạc Cảnh Viên"
+admin.site.site_title = "Hệ thống quản lí Hạc Cảnh Viên"
+admin.site.index_title = "Chào mừng bạn đến với hệ thống quản lí - Hạc Cảnh Viên"
--- haccanhvien/models.py
+++ haccanhvien/models.py
@@ -8,6 +8,10 @@ class KhuVuc(models.Model):
     ten_hien_thi = models.CharField("Tên hiển thị", name="ten_hien_thi", max_length=256)
     mieu_ta = models.TextField("Miêu tả", name="mieu_ta")
 
+    class Meta:
+        verbose_name = "Khu vực"
+        verbose_name_plural = "Khu vực"
+
     def __str__(self) -> str:
         return self.ten_hien_thi
 
@@ -19,6 +23,10 @@ class CapDoSanPham(models.Model):
     def __str__(self) -> str:
         return self.ma_cap_do
 
+    class Meta:
+        verbose_name = "Cấp độ sản phẩm"
+        verbose_name_plural = "Cấp độ sản phẩm"
+
 
 class LoaiSanPham(models.Model):
     ma_san_pham = models.CharField("Mã sản phẩm", name="ma_san_pham", primary_key=True, max_length=256)
@@ -27,11 +35,19 @@ class LoaiSanPham(models.Model):
     def __str__(self) -> str:
         return self.ma_san_pham
 
+    class Meta:
+        verbose_name = "Mã sản phẩm"
+        verbose_name_plural = "Mã sản phẩm"
+
 
 class LoaiHang(models.Model):
     ma_hang = models.CharField("Mã hàng", name="ma_hang", primary_key=True, max_length=256)
-    ma_san_pham = models.ForeignKey(LoaiSanPham, on_delete=models.CASCADE)
-    ma_cap_do = models.ForeignKey(CapDoSanPham, on_delete=models.CASCADE)
+    ma_san_pham = models.ForeignKey(LoaiSanPham, on_delete=models.CASCADE, verbose_name="Mã sản phẩm")
+    ma_cap_do = models.ForeignKey(CapDoSanPham, on_delete=models.CASCADE, verbose_name="Mã cấp độ")
+
+    class Meta:
+        verbose_name = "Loại hàng"
+        verbose_name_plural = "Loại hàng"
 
     def __str__(self) -> str:
         return f"{self.ma_hang} | {self.ma_san_pham} | {self.ma_cap_do}"
@@ -39,12 +55,15 @@ class LoaiHang(models.Model):
 
 class Mo(models.Model):
     ma_mo = models.CharField("Mã mộ", name="ma_mo", primary_key=True, max_length=256)
-    ma_khu_vuc = models.ForeignKey(KhuVuc, on_delete=models.RESTRICT)
+    ma_khu_vuc = models.ForeignKey(KhuVuc, on_delete=models.RESTRICT, verbose_name="Mã khu vực")
+    ma_hang = models.ForeignKey(LoaiHang, on_delete=models.RESTRICT, verbose_name="Loại hàng")
     hang = models.IntegerField(verbose_name="Vị trí hàng", name="hang")
     cot = models.IntegerField(verbose_name="Vị trí cột", name="cot")
 
     class Meta:
         unique_together = ["hang", "cot", "ma_khu_vuc"]
+        verbose_name = "Phần mộ"
+        verbose_name_plural = "Phần mộ"
 
     def __str__(self) -> str:
         return f"{self.ma_mo} | Khu: {self.ma_khu_vuc} | Hàng: {self.hang} | Cột: {self.cot}"
