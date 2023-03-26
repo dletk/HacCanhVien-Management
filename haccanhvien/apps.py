300ea7b19bc139eb4e60dddec95c8830ff6d852c
Duc Le <duc.mtle@gmail.com>
2023-03-26 17:15:52 -0400

    Changing the app verbose name

--- haccanhvien/apps.py
+++ haccanhvien/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class HaccanhvienConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'haccanhvien'
+    verbose_name = "Hạc Cảnh Viên"
