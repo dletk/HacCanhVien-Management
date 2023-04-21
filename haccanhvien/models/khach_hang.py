bb9b3c85f0791534e194b2493b275fc6b2f2d4f6
Duc Le <duc.mtle@gmail.com>
2023-04-21 02:33:38 -0400

    Working on modifying the khachhang model

--- haccanhvien/models/khach_hang.py
+++ haccanhvien/models/khach_hang.py
@@ -5,34 +5,43 @@ from .san_pham import Mo
 
 class KhachMua(models.Model):
     ma_khach_hang = models.AutoField(verbose_name="Mã khách hàng", primary_key=True)
-    ten_khach = models.CharField(verbose_name="Têm khách hàng", name="ten_khach_mua", blank=False, max_length=256)
-    CCCD = models.CharField(verbose_name="Số CMND/CCCD", name="CCCD", blank=False, max_length=20)
-    so_dien_thoai = models.CharField(verbose_name="Số điện thoại", name="so_dien_thoai", max_length=20)
-    dia_chi = models.TextField(verbose_name="Địa chỉ", name="dia_chi")
+    ten_khach = models.CharField(verbose_name="Têm khách hàng", blank=False, max_length=256)
+    CCCD = models.CharField(verbose_name="Số CMND/CCCD", blank=False, max_length=20)
+    so_dien_thoai = models.CharField(verbose_name="Số điện thoại", max_length=20)
+    dia_chi = models.TextField(verbose_name="Địa chỉ")
 
     class Meta:
         verbose_name = "Khách mua hàng"
         verbose_name_plural = "Khách mua hàng"
 
+    def __str__(self) -> str:
+        return f"{self.ten_khach} - SDT: {self.so_dien_thoai}"
+
 
 class KhachMat(models.Model):
     ma_khach_hang = models.AutoField(verbose_name="Mã khách hàng", primary_key=True)
-    ten_khach = models.CharField(verbose_name="Tên khách", name="ten_khach", blank=False, max_length=256)
-    ngay_mat = models.DateField(verbose_name="Ngày mất", name="ngay_mat", blank=False)
+    ten_khach = models.CharField(verbose_name="Tên khách", blank=False, max_length=256)
+    ngay_mat = models.DateField(verbose_name="Ngày mất", blank=False)
     mo = models.ForeignKey(Mo, verbose_name="Mộ chôn", blank=False, on_delete=models.CASCADE)
-    thong_tin_khac = models.TextField(verbose_name="Thông tin khác")
+    thong_tin_khac = models.TextField(verbose_name="Thông tin khác", blank=True)
 
     class Meta:
         verbose_name = "Khách sử dụng mộ"
         verbose_name_plural = "Khách sử dụng mộ"
 
+    def __str__(self) -> str:
+        return f"{self.ten_khach} - Ngày mất: {self.ngay_mat}"
+
 
 class KyUc(models.Model):
-    ma_khach_hang = models.OneToOneField(KhachMat, on_delete=models.CASCADE, verbose_name="Mã khách hàng")
+    khach_hang = models.OneToOneField(KhachMat, on_delete=models.CASCADE, verbose_name="Khách hàng")
 
     class Meta:
         verbose_name = "Ký ức"
         verbose_name_plural = "Ký ức"
+
+    def __str__(self) -> str:
+        return f"Ký ức Khách: {self.khach_hang}"
 
 
 class AnhKyUc(models.Model):
